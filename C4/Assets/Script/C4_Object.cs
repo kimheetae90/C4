85a3f2c94165eda82b3edb4372a65a55de8644fe
kimheetae <kimheetae90@gmail.com>
2015-03-06 00:51:58 +0900

    Object Class

---
+++ C4/Assets/Script/C4_Object.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C4_Object : MonoBehaviour {
+
+    private byte objectID;
+    private byte attributeCode;
+
+    public byte getObjectID()
+    {
+        return objectID;
+    }
+
+    public void setObjectID(byte value)
+    {
+        objectID = value;
+    }
+
+    public byte getAttributeCode()
+    {
+        return attributeCode;
+    }
+
+    public void setAttributeCode(byte value)
+    {
+        attributeCode = value;
+    }
+}
