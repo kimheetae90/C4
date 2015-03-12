d5acf2981475f6e7dd6898dd6e8c4b9ff4a322c5
LeeHyungJung <lhj5772@gmail.com>
2015-03-13 01:27:54 +0900

    yEd Parse

---
+++ C4/Assets/Script/Tool/AI/BehaviorImportTool.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+public class BehaviorImportTool : EditorWindow
+{
+	// Use this for initialization
+    List<string> listXmlFile = new List<string>();
+
+    bool bNeedUpdateRawDataPath = true;
+    int selectedIndex = 0;
+    string basePathToLoad = Application.dataPath + "/Data/AI/Raw";
+    string basePathToSave = Application.dataPath + "/Data/AI/";
+   
+    BehaviorRawDataParser parser = new BehaviorRawDataParser();
+
+    [MenuItem("Window/AI/Behavior Import Tool")]
+    public static void ShowWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(BehaviorImportTool));
+    }
+
+    void Update()
+    {
+        ReloadProcess();
+    }
+
+    void ReloadProcess()
+    {
+        if (bNeedUpdateRawDataPath)
+        {
+            ReloadRawDataDirectory();
+            bNeedUpdateRawDataPath = false;
+        }
+    }
+
+    void OnHierarchyChange()
+    {
+        bNeedUpdateRawDataPath = true;
+    }
+
+    private void ReloadRawDataDirectory()
+    {
+        listXmlFile.Clear();
+        var info = new DirectoryInfo(basePathToLoad);
+        var fileInfo = info.GetFiles();
+        foreach (FileInfo file in fileInfo)
+        {
+            if (file.Extension == ".xgml")
+            {
+                listXmlFile.Add(file.FullName);
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        ShowParserUI();
+    }
+
+    void ShowParserUI()
+    {
+        GUILayout.Label("GXML 파일을 로드해주세요", EditorStyles.boldLabel);
+        selectedIndex = EditorGUILayout.Popup(selectedIndex, listXmlFile.ToArray());
+        if (GUILayout.Button("Load"))
+        {
+            ProcLoadBtn();
+        }
+    }
+
+    void ProcLoadBtn()
+    {
+        if (listXmlFile.Count > 0)
+        {
+            ParseRawData();
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("파일 읽기 실패",
+                  "읽을 파일이 존재하지 않습니다.", "OK");
+        }
+    }
+
+    private void ParseRawData()
+    {
+        string targetPath = listXmlFile[selectedIndex];
+        if (parser.Parse(targetPath))
+        {
+            EditorUtility.DisplayDialog("파일 읽기 성공",
+                        "파일이 올바르게 저장되었습니다. 파일명 : " + targetPath, "OK");
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("파일 읽기 실패",
+                        "파일 읽기에 실패하였습니다. 파일명 : " + targetPath, "OK");
+        }
+    }
+}
---
+++ C4/Assets/Script/Tool/AI/BehaviorRawDataParser.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Xml;
+
+public class BehaviorRawDataParser
+{
+    public BehaviorRawDataParser()
+    {
+
+    }
+
+    void clear()
+    {
+
+    }
+
+    XmlElement LoadXML(string targetpath)
+    {
+        var sr = new StreamReader(targetpath);
+        XmlDocument xmldoc = new XmlDocument();
+        xmldoc.Load(sr);
+        XmlElement root = xmldoc.DocumentElement;
+        return root;
+    }
+
+    public bool Parse(string targetpath)
+    {
+        XmlElement root = LoadXML(targetpath);
+        XmlNodeList nodes = root.ChildNodes;
+
+        foreach(XmlNode node in nodes)
+        {
+            readSection(node);
+        }
+
+        return true;
+    }
+
+    void readSection(XmlNode node)
+    {
+        if (node.Name != "section" && node.Attributes["name"] == null ) return;
+
+        string value = node.Attributes["name"].Value;
+
+        switch (value)
+        {
+            case "graph":
+                {
+                    readGraph(node);
+                }
+                break;
+            case "node":
+                {
+                    readNode(node);
+                }
+                break;
+            case "edge":
+                {
+                    readEdge(node);
+                }
+                break;
+        }
+    }
+
+    void readGraph(XmlNode node)
+    {
+        XmlNodeList childNodes = node.ChildNodes;
+        foreach (XmlNode childNode in childNodes)
+        {
+            readSection(childNode);
+        }
+    }
+
+    void readNode(XmlNode node)
+    {
+        BehaviorRawNodeData data = new BehaviorRawNodeData();
+        XmlNodeList childNodes = node.ChildNodes;
+        foreach (XmlNode childNode in childNodes)
+        {
+            parseNode(childNode,data);
+        }
+    }
+
+    void parseNode(XmlNode node, BehaviorRawNodeData data)
+    {
+        switch(node.Name)
+        {
+            case "attribute":
+                {
+                    parseNodeAttribute(node, data);
+                }
+                break;
+            case "section":
+                {
+                    if (node.Attributes["name"] != null && node.Attributes["name"].Value == "graphics")
+                    {
+                        XmlNodeList childNodes = node.ChildNodes;
+                        foreach (XmlNode childNode in childNodes)
+                        {
+                            parseNodeAttribute(childNode, data);
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    void parseNodeAttribute(XmlNode node, BehaviorRawNodeData data)
+    {
+        if (node.Name != "attribute" && node.Attributes["key"] == null) return;
+
+        string key = node.Attributes["key"].Value;
+
+        switch (key)
+        {
+            case "id":
+                {
+                    //set
+                    Debug.Log("asdf");
+                }
+                break;
+            case "label":
+                {
+                    //set
+                    Debug.Log("asdf");
+                }
+                break;
+            case "type":
+                {
+                    //set
+                    Debug.Log("asdf");
+                }
+                break;
+        }
+    }
+
+    //concrete function
+    void readEdge(XmlNode node)
+    {
+        BehaviorRawEdgeData data = new BehaviorRawEdgeData();
+        XmlNodeList childNodes = node.ChildNodes;
+        foreach (XmlNode childNode in childNodes)
+        {
+            parseEdge(childNode, data);
+        }
+    }
+
+    void parseEdge(XmlNode node, BehaviorRawEdgeData data)
+    {
+        if (node.Attributes["key"] == null) return;
+
+        string key = node.Attributes["key"].Value;
+
+        switch (key)
+        {
+            case "source":
+                {
+                    //set
+                    Debug.Log("asdf");
+                }
+                break;
+            case "target":
+                {
+                    //set
+                    Debug.Log("asdf");
+                }
+                break;
+        }
+    }
+}
