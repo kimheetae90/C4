0af03dd31fa85b812c67ff3c5b2d91fa3d79b1d2
LeeHyungJung <lhj5772@gmail.com>
2015-03-16 01:22:05 +0900

    BehaviorTree Interface

---
+++ C4/Assets/Script/AI/BehaviorNode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BehaviorNode : IBehaviorNode
+{
+    public GameObject targetObject;
+    public BehaviorNode parents;
+    public List<BehaviorNode> listChild;
+
+    public BehaviorNode(GameObject targetObject)
+    {
+        this.targetObject = targetObject;
+        parents     = null;
+        listChild   = new List<BehaviorNode>();
+    }
+
+    virtual public bool traversalNode()
+    {
+        return false;
+    }
+
+    virtual public object Clone()
+    {
+        return null;
+    }
+    
+}
---
+++ C4/Assets/Script/AI/IBehaviorNodeInterface.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public interface IBehaviorNode : ICloneable
+{
+    bool traversalNode();
+}
