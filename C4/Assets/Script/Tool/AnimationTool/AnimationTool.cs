a7df8bd1fff3d1f94ebd4537fa08f75ee65d2fe7
LeeHyungJung <lhj5772@gmail.com>
2015-04-01 00:18:04 +0900

    tool base

---
+++ C4/Assets/Script/Tool/AnimationTool/AnimationTool.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+//씬생성
+//오브젝트배치
+//저장
+public class AnimationTool : EditorWindow
+{
+    // Use this for initialization
+    public List<string> listFBXFile = new List<string>();
+
+    bool needUpdateFBXDataPath = true;
+    bool isOpenEditSceen = false;
+    int selectedFBXFileIndex = 0;
+    string[] listFBXFilePath = { "" };
+
+    public static string basePathToLoad = Application.dataPath + "/Data/AI/Raw";
+
+    [MenuItem("Window/Animation/Animation Event Tool")]
+    public static void showWindow()
+    {
+        //Show existing window instance. If one doesn't exist, make one.
+        var window = EditorWindow.GetWindow(typeof(AnimationTool));
+        window.position = new Rect(200, 200, 200 , 200);
+    }
+
+    void Update()
+    {
+        reloadProcess();
+    }
+
+    void reloadProcess()
+    {
+        if (needUpdateFBXDataPath)
+        {
+            reloadFBXDirectory();
+            needUpdateFBXDataPath = false;
+        }
+    }
+
+    private void reloadFBXDirectory()
+    {
+        listFBXFile.Clear();
+        var info = new DirectoryInfo(basePathToLoad);
+        var fileInfo = info.GetFiles();
+        foreach (FileInfo file in fileInfo)
+        {
+            if (file.Extension == ".fbx")
+            {
+                listFBXFile.Add(file.FullName);
+            }
+        }
+
+        listFBXFilePath = listFBXFile.ToArray();
+    }
+
+    void OnProjectChange()
+    {
+        needUpdateFBXDataPath = true;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Import", EditorStyles.boldLabel);
+        GUILayout.Label("Select FBX File", EditorStyles.boldLabel);
+        selectedFBXFileIndex = EditorGUILayout.Popup(selectedFBXFileIndex, listFBXFilePath, EditorStyles.popup);
+
+        if (GUILayout.Button("Select"))
+        {
+            procLoadBtn();
+        }
+
+        if (GUILayout.Button("Save"))
+        {
+            isOpenEditSceen = false;
+        }
+    }
+
+    void procLoadBtn()
+    {
+        if (listFBXFile.Count > 0)
+        {
+            EditorApplication.NewScene();
+            isOpenEditSceen = true;
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("파일 읽기 실패",
+                  "읽을 파일이 존재하지 않습니다.", "OK");
+        }
+    }
+}
+#endif
