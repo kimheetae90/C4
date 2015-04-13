e0061212eca3ec76af6610e337ba821b02b92dca
gossi <go.byungok@gmail.com>
2015-04-14 00:52:14 +0900

    buttonRefix

---
+++ C4/Assets/Script/Component/UI/C4_AllyButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C4_AllyButton : MonoBehaviour {
+
+
+    public GameObject myCharacter;
+	// Use this for initialization
+    public void selectAlly()
+    {
+
+        C4_GameManager.Instance.sceneMode.GetComponentInChildren<C4_PlayMode>().allyController.selectClickObject(myCharacter);
+    }
+
+    public void movetoselect()
+    {
+        Camera.main.GetComponentInParent<C4_PlaySceneCamera>().moveToSomeObject();
+    }
+}
