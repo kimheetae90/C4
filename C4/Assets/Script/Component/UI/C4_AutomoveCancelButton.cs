023d9d0081cd3c5175df60acf518e2a1cc331499
gossi <go.byungok@gmail.com>
2015-04-15 23:29:21 +0900

    automovecancle

---
+++ C4/Assets/Script/Component/UI/C4_AutomoveCancelButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class C4_AutomoveCancelButton : MonoBehaviour {
+
+    
+   	// Use this for initialization
+	void Start () {
+        this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+
+    public void showButton()
+    {
+        this.gameObject.SetActive(true);
+        
+    }
+
+    public void hideButton()
+    {
+        this.gameObject.SetActive(false);
+    }
+    
+}
---
+++ C4/Assets/Script/Component/UI/C4_AutomoveCancleUI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C4_AutomoveCancleUI : MonoBehaviour {
+
+    C4_Unit unit;
+    C4_UnitFeature unitFeature;
+    C4_Move moveScript;
+    C4_AutomoveCancelButton cancelbt;
+
+	// Use this for initialization
+	void Start () {
+        unit = this.GetComponentInParent<C4_Unit>();
+        unitFeature = this.GetComponentInParent<C4_UnitFeature>();
+        moveScript = this.GetComponentInParent<C4_Move>();
+        cancelbt = this.GetComponentInChildren<C4_AutomoveCancelButton>();
+	}
+	
+	// Update is called once per frame
+    
+    public void startAutomoveCancleUI()
+    {
+        if (unit.canActive == false && Vector3.Distance(moveScript.toMove, unit.transform.position) > 0)
+        {
+            cancelbt.showButton();
+        }
+        if (Vector3.Distance(moveScript.toMove, unit.transform.position) > unitFeature.moveRange)
+        {
+            cancelbt.showButton();
+        }
+        StartCoroutine("automoveCancleUI");
+    }
+    IEnumerator automoveCancleUI()
+    {
+        yield return null;
+
+        
+        if (Vector3.Distance(moveScript.toMove, unit.transform.position) < unitFeature.moveSpeed * 0.02f)
+        {
+            cancelbt.hideButton();
+            StopCoroutine("automoveCancleUI");
+        }
+
+        StartCoroutine("automoveCancleUI");
+    }
+}
