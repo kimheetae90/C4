3fd0f3b0921258230e56ad09a456965f766e0b69
kimheetae <kimheetae90@gmail.com>
2015-05-01 16:37:15 +0900

    dd

--- C4/Assets/Script/Object/Etc/C4_Water.cs
+++ C4/Assets/Script/Object/Etc/C4_Water.cs
@@ -6,9 +6,8 @@ public class C4_Water : C4_Ground
 
     protected override void Awake()
     {
-        base.Awake();
-        C4_Object me = this;
-        C4_GameManager.Instance.objectManager.registerObjectToAll(ref me, GameObjectType.Ground, GameObjectInputType.CameraMoveAbleObject | GameObjectInputType.ToMoveAbleObject);
-    }
-
+		base.Awake ();
+		C4_Object me = GetComponent<C4_Object>();
+		C4_GameManager.Instance.objectManager.registerObjectToAll(ref me, GameObjectType.Ground, GameObjectInputType.CameraMoveAbleObject | GameObjectInputType.ToMoveAbleObject);
+	}
 }
