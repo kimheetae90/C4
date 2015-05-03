7594e0cf83e1ebc6a6828007f84ae336e138effc
LeeHyungJoong <lhj5772@gmail.com>
2015-05-03 22:10:25 +0900

    Modify Resource Path
    and AnimationTool Refactoring

---
+++ C4/Assets/Script/System/Animation/Tool/AnimationEventParam/AnimationEventParamBase.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AnimationEventParamBase
+{	
+	#if UNITY_EDITOR 
+	List<ParamControlBase> ListControl;
+	#endif
+
+	#if UNITY_EDITOR 
+	public AnimationEventParamBase(string functionName)
+	{
+
+	}
+	#else
+	public AnimationEventParamBase()
+	{
+		
+	}
+	#endif
+
+	public virtual void Serialize()
+	{
+
+	}
+
+	public virtual void Deseralize()
+	{
+		
+	}
+
+	#if UNITY_EDITOR 
+	public virtual void InitParamControl()
+	{
+		ListControl = new List<ParamControlBase> ();
+
+	}
+
+	public void OnShowParamControls(int parentWidth, int parentHeight)
+	{
+		for (int i = 0; i < ListControl.Count; ++i) 
+		{
+			ListControl[i].Show(parentWidth,parentHeight);
+		}
+	}
+
+	public virtual void OnDrawEditControl(int windowId)
+	{
+		for (int i = 0; i < ListControl.Count; ++i) 
+		{
+			if(ListControl[i].Editing)
+			{
+				ListControl[i].ShowEditingWindow(windowId);
+				break;
+			}
+		}
+	}
+	#endif
+}
---
+++ C4/Assets/Script/System/Animation/Tool/AnimationEventParam/ParameterControl/ParamControlBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParamControlBase {
+
+	public bool Editing;
+
+	public void Show(int parentWidth,int parentHeight)
+	{
+
+	}
+
+	public void ShowEditingWindow(int windowId)
+	{
+
+	}
+}
