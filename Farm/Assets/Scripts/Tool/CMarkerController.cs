66e8cf81162f8ee3c681df6a8a4e3adebca0d167
kimheetae <kimheetae90@gmail.com>
2015-11-11 16:39:40 +0900

    tool gg

---
+++ Farm/Assets/Scripts/Tool/CMarkerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CMarkerController : Controller {
+
+	List<GameObject> markerPrefabList;
+
+	void Awake()
+	{
+		markerPrefabList = new List<GameObject> ();
+
+		for (int i=0; i<10; i++) 
+		{
+			string prefabName = "marker" + i.ToString();
+			GameObject prefab = Resources.Load("Tool/Prefabs/" + prefabName);
+		}
+	}
+
+	protected override void Start ()
+	{
+		base.Start ();
+	}
+	
+	public override void DispatchGameMessage (GameMessage _gameMessage)
+	{
+
+	}
+
+}
